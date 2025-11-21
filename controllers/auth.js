bc275a9e8899d2d2b2a6898c98b8dc8789595e8f
Tobi <obscurityincarnate@gmail.com>
2025-11-21 19:40:46 +0000

    Validation for if confirm password and password field match

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,20 @@
 import express from 'express';
+
+// * Model 
 import USER from '../models/user.js';
 
 const router = express.Router();
 
-
-router.post("/sign-up",(req,res)=>{
-
+//* Routes
+router.post("/sign-up",async(req,res)=>{
+    console.log(req.body);
+    try {
+        const user = await USER.create(req.body);
+        res.status(200).json(user)
+    } catch (error) {
+        console.log(error)
+        res.json(error)
+    }
 })
 
 router.post("/sign-in", (req,res)=>{
