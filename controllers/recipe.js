e75c2d5f773955a1b4ba56d5ea4fe3e84212f3d6
Natalya Nassimi <natalya1ahronee@gmail.com>
2025-11-24 11:31:23 +0000

    create, index, show and update routes

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -2,24 +2,64 @@ import express from 'express';
 import RECIPES from '../models/recipe.js';
 const router = express.Router();
 
-router.get("", (req,res)=>{
+// ? Create
+router.post("", async (req, res) => {
+    try {
+        // req.body.author = req.user._id
+        const recipe = await RECIPES.create(req.body)
+        res.status(201).json(recipe)
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+// ? Index
+router.get("", async (req, res) => {
+    try {
+        const recipes = await RECIPES.find().populate('author')
+        res.json(recipes)
+    } catch (error) {
+        console.log(error)
+    }
 
 });
-router.post("", (req,res)=>{
 
-});
-router.get("/:recipeId", (req, res)=>{
+//? Show Page
+router.get("/:recipeId", async (req, res) => {
+    try {
+        const { recipeId } = req.params
+        const recipe = await RECIPES.findById(recipeId).populate(['author', 'comments.commenter'])
+        if (!recipe) throw new Error('Recipe not found')
+        res.json(recipe)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// ? Update
+router.put("/:recipeId", async (req, res) => {
+    try {
+        const { recipeId } = req.params
+        const recipe = await RECIPES.findById(recipeId)
+        if (!recipe) throw new Error('Recipe not found')
+
+        // add in when user is defined
+        // if(!recipe.author.equals(req.user._id)) {
+        // throw new Error('No permission to access') }
+
+        const updatedRecipe = await RECIPES.findByIdAndUpdate(recipeId, req.body, { returnDocument: 'after' })
+        res.json(updatedRecipe)
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.delete("/recipeId", (req, res) => {
 
 })
-router.put("/:recipeId", (req, res)=>{
+router.post("/:recipeId/reviews", (req, res) => {
 
-})
-
-router.delete("/recipeId", (req,res)=>{
-
-})
-router.post("/:recipeId/reviews", (req, res)=>{
-    
 })
 
 export default router;
