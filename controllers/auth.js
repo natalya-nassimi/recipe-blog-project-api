de7e6a2586399f9f281e7b10beb158175ab4d442
Tobi <obscurityincarnate@gmail.com>
2025-11-24 11:15:30 +0000

    completed auth routes without error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
 // * Model 
 import USER from '../models/user.js';
 
@@ -10,14 +11,40 @@ router.post("/sign-up",async(req,res)=>{
     console.log(req.body);
     try {
         const user = await USER.create(req.body);
-        res.status(200).json(user)
+        res.status(201).json(user)
     } catch (error) {
         console.log(error)
         res.json(error)
     }
 })
 
-router.post("/sign-in", (req,res)=>{
+router.post("/sign-in", async (req,res)=>{
+    try {
+        const {username, password} =  req.body;
+        const user =  await USER.findOne({username: username});
+        if(!user){
+            throw new Error("Username not Found");
+        }
+        console.log(bcrypt.compareSync(password, user.password))
+        if(!bcrypt.compareSync(password, user.password)){
+            throw new Error("Incorrect Password");
+        }
 
+        const token = jwt.sign(
+            {
+                user: {
+                    _id: user._id,
+                    username: user.username,
+                } 
+            },
+            process.env.TOKEN_SECRET,
+            {expiresIn: "7d"}
+        );
+        
+        res.json(token);
+    } catch (error) {
+        console.log(error)
+        res.json(error);
+    }
 })
 export default router;
