24fbd30183383703fef0f5289d631cce5800ba93
Tobi <obscurityincarnate@gmail.com>
2025-11-26 15:03:11 +0000

    added in validation on measurement and preparation time fields. made preparation time a required field

--- models/recipe.js
+++ models/recipe.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ingredient = {
     name: {type: String, required: true},
-    measurement: {type: Number, required: true},
+    measurement: {type: Number, required: true, min:[0, "The measurement of the ingredient cannot be negative"]},
     unit: {type: String, required: true, enum:['cup', 'gallon', 'gram', 'litre', 'kilogram', 'ounce', 'quart', 'tbsp']}
 }
 const comment = new mongoose.Schema({
@@ -14,7 +14,7 @@ const comment = new mongoose.Schema({
 const recipe = new mongoose.Schema({
     name: {type: String, required: true},
     ingredients: {type:[ingredient], required: true},
-    preparationTime: {type: Number},
+    preparationTime: {type: Number, min:[0, "The preparation time cannot be negative"], required: true},
     instructions: {type: [String], required: true},
     image: {type: String},
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
